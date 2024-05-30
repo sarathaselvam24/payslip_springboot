b3a7d844c2371970cad2afc181cdf15666c0daad
sarathaselvam24 <sarathaselvam24@gmail.com>
2024-05-31 00:32:36 +0530

    Api Calls Added For Employee Loan History

--- src/main/java/com/nulogic/payslip/process/controller/EmployeeController.java
+++ src/main/java/com/nulogic/payslip/process/controller/EmployeeController.java
@@ -1,8 +1,16 @@
 package com.nulogic.payslip.process.controller;
-
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Optional;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -23,15 +31,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nulogic.payslip.process.model.Basicpay;
 import com.nulogic.payslip.process.model.Employee;
 import com.nulogic.payslip.process.model.EmployeeAccountDetails;
+import com.nulogic.payslip.process.model.Loan;
 import com.nulogic.payslip.process.model.Salarydetails;
+import com.nulogic.payslip.process.repository.BasicpayRepository;
 import com.nulogic.payslip.process.repository.EmployeeAccountDetailsRepository;
 import com.nulogic.payslip.process.repository.EmployeeRepository;
+import com.nulogic.payslip.process.repository.LoanRepository;
 import com.nulogic.payslip.process.repository.SalarydetailsRepository;
 
 
@@ -45,6 +59,9 @@ public class EmployeeController {
     private EmployeeAccountDetailsRepository accountDetailsRepository;
     
     @Autowired
+    private BasicpayRepository basicRepo;
+    
+    @Autowired
     private SalarydetailsRepository salaryRepo;
     
     @Autowired
@@ -52,6 +69,9 @@ public class EmployeeController {
     
     @Autowired
     private JavaMailSender emailSender;
+    
+    @Autowired
+    private LoanRepository loanRepo;
 
     public TemplateEngine getTemplateEngine() {
 		return templateEngine;
@@ -83,7 +103,7 @@ public class EmployeeController {
     }
     
     @PostMapping("/api/saveEmployeeAccountDetails")
-    public ResponseEntity<EmployeeAccountDetails> saveEmployeeAccountDetails( Model model,@RequestBody EmployeeAccountDetails accountDetails) {
+    public ResponseEntity<EmployeeAccountDetails> saveEmployeeAccountDetails(@RequestBody EmployeeAccountDetails accountDetails) {
         EmployeeAccountDetails savedAccountDetails = accountDetailsRepository.save(accountDetails);
         return ResponseEntity.ok(savedAccountDetails);
     }
@@ -153,6 +173,104 @@ public class EmployeeController {
         }
     }
     
+	@PostMapping("/api/loanrequest")
+	public ResponseEntity<Boolean> saveLoanRequest(@RequestBody Map<String, String> request) {
+		System.out.println("saveLoanRequest called ");
+		String emistartsfrom = request.get("emiStartsFrom");
+		String expectedMonth = request.get("expectedMonth"); 
+		BigDecimal loanAmount = new BigDecimal(request.get("loanAmount"));
+		BigDecimal repaymentTerms = new BigDecimal(request.get("repaymentTerms"));
+		BigDecimal emi = new BigDecimal(request.get("emi"));
+		String note = request.get("note"); 
+		String employeeid = request.get("empid");
+	    List<Loan> onGoingLoanRequest = loanRepo.findByEmpidAndLoanRequestStatus(employeeid, "On going", "Not Started");
+	    
+	    if (!onGoingLoanRequest.isEmpty()) {
+	        return new ResponseEntity<>(false, HttpStatus.OK);  
+	    } else {
+	        Loan loanRequest = new Loan();
+	        Basicpay ctcrepo = basicRepo.findByEmpid(employeeid);
+	        if (ctcrepo != null) {
+	            loanRequest.setEmpid(ctcrepo.getEmpid());
+	            loanRequest.setEmailid(ctcrepo.getEmailid());
+	            loanRequest.setBasicpay(ctcrepo.getCtc().multiply(BigDecimal.valueOf(40).divide(BigDecimal.valueOf(100)))
+	                                   .divide(BigDecimal.valueOf(12), MathContext.DECIMAL128).setScale(2, RoundingMode.HALF_UP));
+	            loanRequest.setLoanamount(loanAmount);
+	            loanRequest.setExpectedmonth(formatMonthAndYear(expectedMonth));
+	            loanRequest.setEmistartsfrom(formatMonthAndYear(emistartsfrom));
+	            loanRequest.setNote(note);
+	            loanRequest.setIssuedon(Date.valueOf(LocalDate.now()));
+	            loanRequest.setLoanrequeststatus("Not Started");
+	            loanRequest.setLoanstatus("Pending");
+	            loanRequest.setRemainingbalance(loanAmount);
+	            loanRequest.setRepaymentterms(repaymentTerms);
+	            loanRequest.setRequestedby(employeeRepository.findByEmpid(employeeid).get().getName());
+	            loanRequest.setEmi(emi);
+	            Loan createdLoan = loanRepo.save(loanRequest);
+	            if(null != createdLoan) {
+	            	 return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
+	            }else {
+	            	 return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+	            }
+	        }
+	    }
+		return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+	   
+	}
+	
+	@PostMapping("/api/employeeloanrequest")
+	public ResponseEntity<List<Loan>> employeeLoanRequestByEmpid(@RequestBody Map<String, String> request) {
+		System.out.println("employeeLoanRequestByEmpid called ");
+		String empid = request.get("empid");
+	    Optional<List<Loan>> employeeLoanRequests = loanRepo.findByEmpid(empid);
+	   
+	    return employeeLoanRequests.map(ResponseEntity::ok)
+	            .orElseGet(() -> ResponseEntity.notFound().build());
+	   
+	}
+	
+	@PostMapping("/api/editEmployeeLoanStatus")
+	public ResponseEntity<Boolean> editEmployeeLoanStatus(@RequestBody Map<String, String> request) {
+		int loanId = Integer.valueOf(request.get("loanid"));
+			
+		String loanRequestStatus = request.get("loanrequeststatus");
+		String loanCanceledBy = request.get("canceledby");
+	    Loan loan = loanRepo.findById(loanId).orElseThrow(() -> new IllegalArgumentException("Invalid loan Id:" + loanId));
+	    
+	   
+	    if(loanRequestStatus.equalsIgnoreCase("Canceled")) {
+	    	loan.setLoanstatus(loanRequestStatus);
+	    	loan.setCanceledby(loanCanceledBy);
+	    }
+	    loanRepo.save(loan);
+	    return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+	
+	@PostMapping("/api/searchMyLoanRequestByStatus")
+	public ResponseEntity<List<Loan>> searchMyLoanRequestByStatus(@RequestBody Map<String, String> request) {
+		
+		String search = request.get("searchTerm");
+		String empid = request.get("empid");
+		System.out.println("search "+search);
+		System.out.println("empid "+empid);
+	    if (search == null || search.isEmpty()) {
+	    	Optional<List<Loan>> employeeAllLoanRequests = loanRepo.findByEmpid(empid);
+	    	System.out.println("searchMyLoanRequestByStatus if called "+employeeAllLoanRequests);
+		    return employeeAllLoanRequests.map(ResponseEntity::ok)
+		            .orElseGet(() -> ResponseEntity.notFound().build());
+	    } else {
+	    	Optional<List<Loan>> employeeLoanRequestsByStatus = loanRepo.findByLoanstatusContaining(search);
+	    	System.out.println("searchMyLoanRequestByStatus else called "+employeeLoanRequestsByStatus);
+		    return employeeLoanRequestsByStatus.map(ResponseEntity::ok)
+		            .orElseGet(() -> ResponseEntity.notFound().build());
+	    }
+	   
+	  
+	}
+
+    
+    
+    
     private String generateHtmlContent(Model model) {
 		Context context = new Context();
 		model.asMap().forEach(context::setVariable);
@@ -192,5 +310,20 @@ public class EmployeeController {
 	            e.printStackTrace();
 	        }
 	    }
+	   
+	   private Date formatMonthAndYear(String expectedmonth) {
+			try {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+				YearMonth yearMonth = YearMonth.parse(expectedmonth, formatter);
+				LocalDate firstDayOfMonth = yearMonth.atDay(1);
+				Date parseDate = Date.valueOf(firstDayOfMonth);
+				System.out.println("month " + Date.valueOf(firstDayOfMonth));
+				return parseDate;
+			} catch (DateTimeParseException e) {
+				e.printStackTrace();
+				return null;
+			}
+
+		}
 
 }
